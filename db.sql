c576e96b28b53b04bc067d8b1bfd338a6945c2f4
gary2504 <garychaves25@gmail.com>
2024-08-25 22:19:36 -0600

    Actualizacion a resolucion de conflictos

--- db.sql
+++ db.sql
@@ -51,15 +51,6 @@ CREATE TABLE Reserva (
     FOREIGN KEY (id_cliente) REFERENCES Cliente(id_cliente),
     Foreign Key (habitacion_id) REFERENCES Habitacion(habitacion_id)
 );
-INSERT INTO Cliente (nombre_cliente, apellido_cliente, ced_id, correo, celular,id_pais)
-VALUES 
-("Laura", "Lopez", 123456789, "lauralopez@gmail.com", "8765-4321",1),
-("Carlos", "Fernandez", 776543212, "carlosfernandez@gmail.com", "2456-7890",2),
-("Sofia", "Rodriguez", 354764367, "sofiarodriguez@gmail.com", "6254-6975",3),
-("Diego", "Martinez", 538511353, "dmartinez@gmail.com", "7245-6843",4),
-("Daddy", "Yankee", 652348713, "digua@gmail.com", "6236-7534",5),
-("Andrea", "Solano", 542672842, "andreasolano@gmail.com", "4312-5436",6);
-
 
 INSERT INTO Pais (nombre_pais, pais_tel_codigo)
 VALUES 
@@ -69,17 +60,6 @@ VALUES
 ("España", "+34"),
 ("USA", "+1"),
 ("Panamá", "+507");
-
-INSERT INTO Habitacion (id_hotel,numero_habitacion,tipo_habitacion,estado,precio_noche,capacidad_maxima) 
-VALUES
-(1,1,"familiar","disponible",80000,7),
-(2,2,"individual","disponible",50000,2),
-(3,3,"doble","disponible",65000,4),
-(4,4,"suite","disponible",110000,6),
-(5,5,"departamento","disponible",150000,5),
-(6,6,"suite_presidencial","disponible",280000,5);
-
-
 INSERT INTO Hotel (nombre_hotel,cantidad_habitaciones,cantidad_personas,categoria_estrellas,ubicacion,descripcion,id_pais)
 VALUES 
 ("Hotel Montaña",60,210,4,"Monteverde","Hotel familiar de montaña",1),
@@ -88,12 +68,31 @@ VALUES
 ("Hotel Gran Casino",200,950,5,"Alajuela","Hotel lujoso y exclusivo",4),
 ("Hotel Croc´s",45,180,3,"Jacó","Hotel playero",5),
 ("Hotel San Andres",70,245,5,"Alajuela","Hotel lujoso y exclusivo",6);
+INSERT INTO Habitacion (id_hotel,numero_habitacion,tipo_habitacion,estado,precio_noche,capacidad_maxima) 
+VALUES
+(1,1,"familiar","disponible",80000,7),
+(2,2,"individual","disponible",50000,2),
+(3,3,"doble","disponible",65000,4),
+(4,4,"suite","disponible",110000,6),
+(5,5,"departamento","disponible",150000,5),
+(6,6,"suite_presidencial","disponible",280000,5);
+INSERT INTO Cliente (nombre_cliente, apellido_cliente, ced_id, correo, celular,id_pais)
+VALUES 
+("Laura", "Lopez", 123456789, "lauralopez@gmail.com", "8765-4321",1),
+("Carlos", "Fernandez", 776543212, "carlosfernandez@gmail.com", "2456-7890",2),
+("Sofia", "Rodriguez", 354764367, "sofiarodriguez@gmail.com", "6254-6975",3),
+("Diego", "Martinez", 538511353, "dmartinez@gmail.com", "7245-6843",4),
+("Daddy", "Yankee", 652348713, "digua@gmail.com", "6236-7534",5),
+("Andrea", "Solano", 542672842, "andreasolano@gmail.com", "4312-5436",6);
 
 INSERT INTO Reserva (id_cliente,cantidad_personas,check_in_date,check_out_date,tipo_de_pago,habitacion_id,fecha_reserva,estado)
 VALUES 
-(7,4,'2024-07-01 08:00:00','2024-07-05 12:00:00','Efectivo',1,'2024-06-27 12:00:00',"pendiente"),
-(8,2,'2024-01-01 10:00:00','2024-01-07 12:00:00','Tarjeta',2,'2023-12-20 10:00:00',"cancelada"),
-(9,4,'2024-02-09 08:30:00','2024-02-17 12:00:00','Tarjeta',3,'2024-01-10 16:00:00',"confirmada"),
-(10,1,'2024-03-10 07:00:00','2024-03-15 12:00:00','Efectivo',4,'2024-03-05 13:00:00',"pendiente"),
-(11,3,'2024-04-10 12:00:00','2024-04-17 12:00:00','Tarjeta',5,'2024-04-06 06:00:00',"cancelado"),
-(12,4,'2024-06-01 12:00:00','2024-06-10 12:00:00','Efectivo',6,'2024-05-25 15:00:00',"confirmada");
+(1,4,'2024-07-01 08:00:00','2024-07-05 12:00:00','Efectivo',1,'2024-06-27 12:00:00',"pendiente"),
+(2,2,'2024-01-01 10:00:00','2024-01-07 12:00:00','Tarjeta',2,'2023-12-20 10:00:00',"cancelada"),
+(3,4,'2024-02-09 08:30:00','2024-02-17 12:00:00','Tarjeta',3,'2024-01-10 16:00:00',"confirmada"),
+(4,1,'2024-03-10 07:00:00','2024-03-15 12:00:00','Efectivo',4,'2024-03-05 13:00:00',"pendiente"),
+(5,3,'2024-04-10 12:00:00','2024-04-17 12:00:00','Tarjeta',5,'2024-04-06 06:00:00',"cancelado"),
+(6,4,'2024-06-01 12:00:00','2024-06-10 12:00:00','Efectivo',6,'2024-05-25 15:00:00',"confirmada");
+
+
+DROP DATABASE Proyecto_MySQL_Reservas;
