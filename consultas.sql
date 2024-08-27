9a895b75defc7c16d2525005d9435e03b5ea9851
W-Alexander <alexanderaguilar726@gmail.com>
2024-08-27 10:24:22 -0600

    Creacion trigger que actualiza la disponibilidad de habitaciones

---
+++ consultas.sql
@@ -0,0 +1,9 @@
+--Consulta para contar cuántas habitaciones disponibles hay en un hotel específico en una
+--fecha dada.
+
+--Consulta para calcular el promedio de reservas diarias en un hotel.
+
+--Consulta para identificar el hotel con la mayor ocupación en el mes anterior.
+
+--Consulta para listar los hoteles que tienen habitaciones disponibles pero no han sido
+--reservadas en el último mes.
