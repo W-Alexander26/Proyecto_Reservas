86cd7a97ef3ca0e05a103eb7a676a1dfca954e4a
W-Alexander <alexanderaguilar726@gmail.com>
2024-08-30 08:39:07 -0600

    Ordenamiento de consultas

--- consultas.sql
+++ consultas.sql
@@ -1,14 +1,38 @@
+USE Proyecto_MySQL_Reservas;
+--------------------------------------------------------------------------------------------------------------------------------------
+--Consulta para obtener los hoteles con mayor número de reservas.
+SELECT h.nombre_hotel, COUNT(res.id_reserva) AS total_reservas
+FROM Hotel h
+JOIN Habitacion ha ON h.id_hotel = ha.id_hotel
+JOIN Reserva res ON ha.habitacion_id = res.habitacion_id
+GROUP BY h.nombre_hotel
+ORDER BY total_reservas DESC LIMIT 1;
+--------------------------------------------------------------------------------------------------------------------------------------
+
 --Consulta para contar cuántas habitaciones disponibles hay en un hotel específico en una
 --fecha dada.
-USE Proyecto_MySQL_Reservas;
-
 SELECT h.nombre_hotel AS nombre_hotel, COUNT(ha.habitacion_id) AS habitaciones_disponibles
 FROM hotel h
 JOIN habitacion ha ON h.id_hotel = ha.id_hotel
 LEFT JOIN reserva r ON ha.habitacion_id = r.habitacion_id AND r.check_in_date = '2024-07-01'
 WHERE r.habitacion_id IS NULL
 GROUP BY h.nombre_hotel;
-
+----------------------------------------------------------------------------------------------------------------------------------------
+--Consulta para buscar hoteles por nombre.
+SELECT * FROM Hotel WHERE nombre_hotel LIKE '%Fiesta%'
+-----------------------------------------------------------------------------------------------------------------------------------------
+--Consulta para buscar hoteles cuya ubicación comienza con un texto específico.
+SELECT * FROM Hotel WHERE ubicacion LIKE 'Al%';
+------------------------------------------------------------------------------------------------------------------------------------------
+--Consulta para buscar hoteles cuya ubicación termina con un texto específico.
+SELECT * FROM Hotel WHERE ubicacion LIKE '%a';
+------------------------------------------------------------------------------------------------------------------------------------------
+-- Consulta para obtener las reservas de un cliente (por email) realizadas en el mes anterior.
+SELECT fecha_reserva, nombre_cliente, apellido_cliente, correo, id_reserva, habitacion_id, estado FROM Reserva r
+Join Cliente c on r.id_cliente = c.id_cliente
+where c.correo = "lauralopez@gmail.com"
+AND MONTH(r.fecha_reserva) = MONTH(CURDATE()) - 1;
+-------------------------------------------------------------------------------------------------------------------------------------------
 --Consulta para calcular el promedio de reservas diarias en un hotel.
 SELECT 
   h.nombre_hotel AS nombre_hotel,
@@ -27,7 +51,7 @@ FROM
   ) dr ON h.id_hotel = dr.id_reserva
 GROUP BY 
   h.nombre_hotel;
-
+--------------------------------------------------------------------------------------------------------------------------------------------
 --Consulta para identificar el hotel con la mayor ocupación en el mes anterior.
 SELECT 
   h.nombre_hotel AS nombre_hotel,
@@ -46,7 +70,7 @@ FROM
   ) dr ON h.id_hotel = dr.id_reserva
 GROUP BY 
   h.nombre_hotel;
-
+---------------------------------------------------------------------------------------------------------------------------------------------
 --Consulta para listar los hoteles que tienen habitaciones disponibles pero no han sido
 --reservadas en el último mes.
 SELECT 
@@ -65,19 +89,5 @@ WHERE
       AND r.check_in_date < CURRENT_DATE
   )
   AND ha.estado = 'disponible';
-  SELECT * FROM Hotel WHERE ubicacion LIKE 'Al%';
+------------------------------------------------------------------------------------------------------------------------------------------------
 
-SELECT * FROM Hotel WHERE nombre_hotel LIKE '%Fiesta%'
-
-SELECT h.nombre_hotel, COUNT(res.id_reserva) AS total_reservas
-FROM Hotel h
-JOIN Habitacion ha ON h.id_hotel = ha.id_hotel
-JOIN Reserva res ON ha.habitacion_id = res.habitacion_id
-GROUP BY h.nombre_hotel
-ORDER BY total_reservas DESC LIMIT 1;
-
--- Consulta para obtener las reservas de un cliente (por email) realizadas en el mes anterior.
-SELECT fecha_reserva, nombre_cliente, apellido_cliente, correo, id_reserva, habitacion_id, estado FROM Reserva r
-Join Cliente c on r.id_cliente = c.id_cliente
-where c.correo = "lauralopez@gmail.com"
-AND MONTH(r.fecha_reserva) = MONTH(CURDATE()) - 1;
